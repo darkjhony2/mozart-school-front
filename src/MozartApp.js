4dd569440cb1c1a182d2171a7363246f9519f96e
Johnny Quezada <jhonnyquezada1997@gmail.com>
2022-05-29 18:20:44 -0500

    first commit

---
+++ src/MozartApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MozartApp = (props) => {
+  return (
+    <h1>MozartApp</h1>
+  )
+}
+
+MozartApp.propTypes = {}
+
+export default MozartApp
