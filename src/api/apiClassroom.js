12a679ec7a74b83248f63d06f68c374ee2fd2051
Johnny Quezada <jhonnyquezada1997@gmail.com>
2022-06-08 22:47:10 -0500

    Cambiando urls para el api

--- src/api/apiClassroom.js
+++ src/api/apiClassroom.js
@@ -1,6 +1,6 @@
 import * as connection from './connection'
 
-const URL_API = "https://localhost:7242/"
+const URL_API = "http://190.117.70.171:9981/"
 
 export const list = (year) => {
     let config = {
