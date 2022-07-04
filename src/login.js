bb2942b6a6e2804a549fa30bcb77f962342c5301
Ranzes33 <ranzes33@gmail.com>
2022-07-04 18:47:38 -0500

    Design simple login added

--- src/login.js
+++ src/login.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
-import { Col, Container, Row, Card, Label, Input, Button } from 'reactstrap';
+import {Container} from 'reactstrap';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import * as apiLogin from './api/apiLogin'
+import './assets/style/LoginUi.css';
+import profile from "./assets/img/user.png";
+import email from "./assets/img/email.jpg";
+import pass from "./assets/img/pass.png";
 
 const Login = props => {
 
@@ -12,24 +16,24 @@ const Login = props => {
 
     async function login() {
         let credentials = {};
-        if(user == ""){
+        if (user == "") {
             MySwal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: "El campo usuario esta vacío",
             });
             return;
-        }else{
+        } else {
             credentials.username = user;
         }
-        if(password == ""){
+        if (password == "") {
             MySwal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: "El campo contraseña esta vacío",
             });
             return;
-        }else{
+        } else {
             credentials.password = password;
         }
         let response = await apiLogin.login(credentials);
@@ -42,36 +46,51 @@ const Login = props => {
 
     return (
         <Container fluid className='p-4 bg-smoke' style={{ height: window.innerHeight }}>
-            <Row>
-                <Col sm = "3">
-                    <Card body>
-                        <Row>
-                            <Col sm="6">
-                                <h4><b>Login</b></h4>
-                            </Col>
-                            <hr/>
-                        </Row>
-                        <Row>
-                            <Col sm = "6">
-                                <Label size='sm'>Usuario:</Label>
-                                <Input size={'sm'} value = { user } name= "user" onChange = { e => setUser(e.target.value) } />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col sm = "6">
-                                <Label size='sm'>Contraseña:</Label>
-                                <Input size={'sm'} type='password' name= "password" value = { password } onChange = { e => setPassword(e.target.value) } />
-                            </Col>
-                        </Row>
-                        <Row className='mt-3'>
-                            <Col sm = "2">
-                                <Button size='sm' onClick={ e => login()} >Ingresar</Button>
-                            </Col>
-                        </Row>
-                    </Card>
-                </Col>
-            </Row>
+            <div className="main">
+                <div className="sub-main">
+                    <div>
+                        <div className="imgs">
+                            <div className="container-image">
+                                <img src={profile} alt="profile" className="profile" />
 
+                            </div>
+
+                        </div>
+                        <div>
+                            <h1 className='mb-4'>Login Page</h1>
+                            <div>
+                                <img src={email} alt="email" className="email" />
+                                <input 
+                                    size='sm'
+                                    type="text" 
+                                    placeholder="username" 
+                                    className="name" 
+                                    value={user} 
+                                    name="user" 
+                                    onChange={e => setUser(e.target.value)}
+                                />
+                            </div>
+                            <div className="second-input">
+                                <img src={pass} alt="pass" className="email" />
+                                <input 
+                                    size='sm'
+                                    type="password" 
+                                    placeholder="password" 
+                                    className="name" 
+                                    name="password" 
+                                    value={password} 
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <div className="login-button mt-3">
+                                <button size='sm' onClick={e => login()}>Login</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
         </Container>
     )
 }
